6e3aa3d58ae6654cc377addf1b9f7cb42b5e0a39
Ian McKenzie <imckenzie777@gmail.com>
2020-03-07 17:12:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,5 +27,20 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch (age)
+  switch (age) {
+    case 13:
+    console.log("You are a teenager");
+    break;
+    case 14:
+    console.log("You are a teenager");
+    break;
+    case 15:
+    console.log("You are a teenager");
+    break;
+    case 16:
+    console.log("You are a teenager");
+    break;
+    case 17:
+    console.log("You are a teenager");
+  }
 }
